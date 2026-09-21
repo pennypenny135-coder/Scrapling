1f28e1254ede7d68701bc85cc2df69fe1e390502
pennypenny135-coder <pennypenny135@gmail.com>
2026-09-21 16:25:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 from scrapling import Fetcher
+import json
 
-def GET(request):
-    url = request.args.get('url')
+def handler(scope, receive, send):
+    """ASGI handler for Vercel"""
+    async def run():
+        # 只處理 GET request
+        if scope['type'] == 'http' and scope['method'] == 'GET':
+            # 攞 query string
+            query_string = scope.get('query_string', b'').decode()
+            url_param = ''
+            for param in query_string.split('&'):
+                if param.startswith('url='):
+                    url_param = param[4:]
+            
+            if not url_param:
+                body = json.dumps({'error': 'Missing url parameter'})
+                status = 400
+            else:
+                try:
+                    fetcher = Fetcher()
+                    response = fetcher.get(url_param)
+                    
+                    titles = response.css('h3 a').getall()
+                    links = response.css('h3 a').attribs('href').getall()
+                    
+                    news = []
+                    for t, l in zip(titles, links):
+                        if t and l:
+                            news.append({'title': t, 'link': l})
+                    
+                    body = json.dumps({'status': 200, 'data': news})
+                    status = 200
+                except Exception as e:
+                    body = json.dumps({'error': str(e)})
+                    status = 500
+            
+            # 送 response
+            await send({
+                'type': 'http.response.start',
+                'status': status,
+                'headers': [[b'content-type', b'application/json']],
+            })
+            await send({
+                'type': 'http.response.body',
+                'body': body.encode(),
+            })
     
-    if not url:
-        return {'error': 'Missing url parameter'}, 400
-    
-    try:
-        fetcher = Fetcher()
-        response = fetcher.get(url)
-        
-        titles = response.css('h3 a').getall()
-        links = response.css('h3 a').attribs('href').getall()
-        
-        news = []
-        for t, l in zip(titles, links):
-            if t and l:
-                news.append({'title': t, 'link': l})
-        
-        return {'status': 200, 'data': news}
-    
-    except Exception as e:
-        return {'error': str(e)}, 500
-
-# 加呢條線！
-handler = GET
+    return run()
